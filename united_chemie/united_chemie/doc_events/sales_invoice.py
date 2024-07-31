4a3bc8747b3ae769cbf1d1e0f3590e315f9ba97d
finbyz <info@finbyz.tech>
2024-07-31 16:28:36 +0530

    FOB Changes DB

--- united_chemie/united_chemie/doc_events/sales_invoice.py
+++ united_chemie/united_chemie/doc_events/sales_invoice.py
@@ -7,5 +7,7 @@ def calculate_inr_freight(self):
         if row.freight:
             row.base_freight = row.freight * self.conversion_rate
             row.base_insurance = row.insurance * self.conversion_rate
+            row.base_fob_value = row.fob_value / self.conversion_rate
     self.base_freight = self.freight * self.conversion_rate
     self.base_insurance = self.insurance * self.conversion_rate
+    self.base_fob_value = self.total_fob_value / self.conversion_rate
