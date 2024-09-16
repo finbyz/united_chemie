69192f84cf2b28a2c7ab0bfcbdb70f5bf158da0c
finbyz <info@finbyz.tech>
2024-09-16 14:32:10 +0530

    Journal Entry due date calculation from customer and supplier default payment term to set in Journal Entry NB

--- united_chemie/hooks.py
+++ united_chemie/hooks.py
@@ -27,7 +27,9 @@ app_license = "MIT"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-# doctype_js = {"doctype" : "public/js/doctype.js"}
+doctype_js = {
+    "Journal Entry" : "public/js/journal_entry.js",
+    }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
