7e0a50f99fcadbde5f5bc89c263b6f780a2666e5
Finbyz <info@finbyz.tech>
2025-06-03 15:14:20 +0530

    PSchanges in hooks

--- united_chemie/united_chemie/override/override_doctype_class/custom_sales_order.py
+++ united_chemie/united_chemie/override/override_doctype_class/custom_sales_order.py
@@ -3,12 +3,13 @@ import frappe
 
 class CustomSalesOrder(AccountsController):
     def validate_party_address(self, party, party_type, billing_address, shipping_address=None):
-        if billing_address or shipping_address:
-            party_address = frappe.get_all(
-                "Dynamic Link",
-                {"link_doctype": party_type, "link_name": party, "parenttype": "Address"},
-                pluck="parent",
-            )
+        pass
+        # if billing_address or shipping_address:
+        #     party_address = frappe.get_all(
+        #         "Dynamic Link",
+        #         {"link_doctype": party_type, "link_name": party, "parenttype": "Address"},
+        #         pluck="parent",
+        #     )
             # if billing_address and billing_address not in party_address:
             #     frappe.throw(_("Billing Address does not belong to the {0}").format(party))
             # elif shipping_address and shipping_address not in party_address:
