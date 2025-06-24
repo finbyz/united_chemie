a502da3316059c56b6cd913044fddf8435100dfe
Finbyz <info@finbyz.tech>
2025-06-24 07:31:40 +0000

    change in ball mill data sheet AD

--- united_chemie/hooks.py
+++ united_chemie/hooks.py
@@ -116,7 +116,7 @@ override_doctype_class = {
 	# "ToDo": "custom_app.overrides.CustomToDo",
     "Payment Entry": "united_chemie.united_chemie.override.override_doctype_class.payment_entry.PaymentEntry",
     "Sales Order": "united_chemie.united_chemie.override.override_doctype_class.custom_sales_order.CustomSalesOrder",
-    # "Ball Mill Data Sheet" : "united_chemie.united_chemie.override.override_doctype_class.ball_mill_data_sheet.BallMillDataSheet"
+    "Ball Mill Data Sheet" : "united_chemie.united_chemie.override.override_doctype_class.ball_mill_data_sheet.BallMillDataSheet"
 }
 
 # Document Events
