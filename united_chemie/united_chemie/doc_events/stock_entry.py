0012b8c9d85bbe6cc3758a98c70353a2f5b6cccb
info@finbyz.tech <info#finbyz.tech>
2025-11-25 16:24:35 +0530

    stock entry validatio Done NB

--- united_chemie/united_chemie/doc_events/stock_entry.py
+++ united_chemie/united_chemie/doc_events/stock_entry.py
@@ -7,9 +7,10 @@ import frappe
 #             if row.expense_account:
 #                 expense_account = f"{items.description} - {abbr}"
 #                 row.expense_account = expense_account
+
 def validate(self, method):
     if self.stock_entry_type == "Manufacture" and self.work_order:
         abbr = frappe.db.get_value("Company", self.company, "abbr")
         for row in self.additional_costs:
             if row.expense_account:
-                row.expense_account = f"{row.description} - {abbr}"
+                row.expense_account = f"{row.description}"
